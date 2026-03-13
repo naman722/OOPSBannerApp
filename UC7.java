11638625c902ab534dbefa5ece4772c8b73506a2
naman722 <nj1816@srmist.edu.in>
2026-03-13 23:22:14 +0530

    UC7 Completed

---
+++ UC7.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC7 {
+
+    public static void main(String[] args) {
+
+        Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+        patternMap.put('O', new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        }));
+
+        patternMap.put('P', new CharacterPattern('P', new String[]{
+                " ******",
+                "*     *",
+                "*     *",
+                " ******",
+                "*      ",
+                "*      ",
+                "*      "
+        }));
+
+        patternMap.put('S', new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        }));
+
+        String word = "OOPS";
+
+        int height = 7;
+        String[] banner = new String[height];
+
+        for (int row = 0; row < height; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                CharacterPattern pattern = patternMap.get(ch);
+                if (pattern != null) {
+                    line.append(pattern.getLine(row)).append("   ");
+                }
+            }
+
+            banner[row] = line.toString();
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+
+class CharacterPattern {
+
+    private char character;
+    private String[] pattern;
+
+    public CharacterPattern(char character, String[] pattern) {
+        this.character = character;
+        this.pattern = pattern;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public String getLine(int index) {
+        return pattern[index];
+    }
+}
